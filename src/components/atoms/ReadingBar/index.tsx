b7f9441bbd1fd5f537d919fce10db1bc17fe713a
Stackbit Code Editor <projects@stackbit.com>
2022-04-27 13:16:02 +0000

    index.tsx: changed

--- src/components/atoms/ReadingBar/index.tsx
+++ src/components/atoms/ReadingBar/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ReadingBar = ({ text }) => {
   return (
-    <div id="readingProgress" className="border-yellow squiggle">
+    <div id="reading-progress" className="border-yellow squiggle">
         <div id="reading-progress-fill"></div>
     </div>
   )
