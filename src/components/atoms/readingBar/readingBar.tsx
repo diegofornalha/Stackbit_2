edea495f5debd73f164aaba79478952d33afdc9b
Stackbit Code Editor <projects@stackbit.com>
2022-04-27 12:02:56 +0000

    readingBar.tsx: changed

--- src/components/atoms/readingBar/readingBar.tsx
+++ src/components/atoms/readingBar/readingBar.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import classNames from 'classnames';
 import Link from '../Link';
 import { iconMap } from '../../svgs';
+import 
 
 const readingProgress = document.querySelector("#reading-progress-fill");
 document.addEventListener("scroll", function (e) {
