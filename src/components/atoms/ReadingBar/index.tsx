1190104cd4ba291d5641d32f9b4096faec1b9d6c
Stackbit Code Editor <projects@stackbit.com>
2022-04-27 12:56:33 +0000

    index.tsx: changed

--- src/components/atoms/ReadingBar/index.tsx
+++ src/components/atoms/ReadingBar/index.tsx
@@ -3,11 +3,7 @@ import React from 'react'
 
 const AlertSection = ({ text }) => {
   return (
-    <div className="py-6 bg-primary">
-      <div className="container mx-auto">
-        <p className="text-center text-xl text-white">{text}</p>
-      </div>
-    </div>
+    <div id="reading-progress" class="border-yellow squiggle"><div id="reading-progress-fill"></div></div>
   )
 }
 
