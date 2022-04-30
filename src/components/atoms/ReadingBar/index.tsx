0b46d95ec51c8bf41cc79d9b510a47d138d3484e
Stackbit Code Editor <projects@stackbit.com>
2022-04-30 16:38:41 +0000

    index.tsx: changed

--- src/components/atoms/ReadingBar/index.tsx
+++ src/components/atoms/ReadingBar/index.tsx
@@ -8,7 +8,7 @@ const ReadingBar = ({ text }) => {
   )
 }
 
-const readingProgress = document.querySelector('#reading-progress-fill') HTMLCollectionOf<HTMLElement></HTMLElement>;
+const readingProgress = document.querySelector('#reading-progress-fill') ;
 document.addEventListener('scroll', function(e) {
   let w = (document.body.scrollTop || document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
   readingProgress.style.setProperty('width', w + '%');
