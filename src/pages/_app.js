4ab52736ae1fe934ee10def9c5e337299728a65b
Stackbit Code Editor <projects@stackbit.com>
2022-05-02 15:21:01 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,3 +3,6 @@ import '../css/main.css';
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
 }
+
+
+<!-- Start cookieyes banner --> <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/a1946ef3a72cf26b47f65c67/script.js"></script> <!-- End cookieyes banner -->
