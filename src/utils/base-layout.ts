73b2ec02371057e4d276b5b8bdeba21638778324
Stackbit Code Editor <projects@stackbit.com>
2022-05-02 15:22:47 +0000

    base-layout.ts: changed

--- src/utils/base-layout.ts
+++ src/utils/base-layout.ts
@@ -18,4 +18,3 @@ export function getBaseLayoutComponent(pageBaseLayout, siteConfigBaseLayout) {
 }
 
 
-<!-- Start cookieyes banner --> <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/a1946ef3a72cf26b47f65c67/script.js"></script> <!-- End cookieyes banner -->
