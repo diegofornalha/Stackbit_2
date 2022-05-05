d2c0e6f372d41a988be3961f12533a38768ca4f8
Stackbit Code Editor <projects@stackbit.com>
2022-05-05 18:51:35 +0000

    index.tsx: changed

--- src/components/sections/HeroSection/index.tsx
+++ src/components/sections/HeroSection/index.tsx
@@ -75,7 +75,7 @@ function HeroBody(props) {
                             skipAddStyles: true,
                         }}
                     /></span>
-                    {subtitle2 className=classNames()}
+                    {subtitle2 className={classNames()}
 
                 </p>
             )}
