3d8db13b5a54054b5c282bce81d93af4bf353f29
Stackbit Code Editor <projects@stackbit.com>
2022-05-05 17:43:36 +0000

    index.tsx: changed

--- src/components/molecules/ReactType/index.tsx
+++ src/components/molecules/ReactType/index.tsx
@@ -1,10 +1,10 @@
 import TypeWriterEffect from 'react-typewriter-effect';
 
- <TypeWriterEffect
-            textStyle={{ fontFamily: 'Red Hat Display' }}
-            startDelay={100}
-            cursorColor="black"
-            text="This is a single text"
-            typeSpeed={100}
-            scrollArea={App}
-          />
+<TypeWriterEffect
+    textStyle={{ fontFamily: 'Red Hat Display' }}
+    startDelay={100}
+    cursorColor="black"
+    text="This is a single text"
+    typeSpeed={100}
+    scrollArea={App}
+/>
