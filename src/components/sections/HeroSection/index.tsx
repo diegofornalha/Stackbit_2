53f527b7589ad96d273a2ef5ace12a0bb8e6aeec
Stackbit Code Editor <projects@stackbit.com>
2022-05-05 18:05:11 +0000

    index.tsx: changed

--- src/components/sections/HeroSection/index.tsx
+++ src/components/sections/HeroSection/index.tsx
@@ -68,7 +68,7 @@ function HeroBody(props) {
                     className={classNames('text-xl', 'sm:text-2xl', styles.subtitle ? mapStyles(styles.subtitle) : null, { 'mt-4': title })}
                     data-sb-field-path=".subtitle"
                 >
-                    {subtitle} && ()
+                    {subtitle} && (
                     <span>
                         <Typewriter
                             options={{
