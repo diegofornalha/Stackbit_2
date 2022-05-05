59e12d720e5246dc5afab7a4f160ce66738185a7
Stackbit Code Editor <projects@stackbit.com>
2022-05-05 17:26:14 +0000

    components-registry.ts: changed

--- src/components/components-registry.ts
+++ src/components/components-registry.ts
@@ -61,5 +61,6 @@ const components = {
     'PostFeedCategoryLayout': dynamic(() => import('./layouts/PostFeedCategoryLayout')),
     'ProjectLayout': dynamic(() => import('./layouts/ProjectLayout')),
     'ProjectFeedLayout': dynamic(() => import('./layouts/ProjectFeedLayout')),
+    
     // 'ReadingBar': dynamic(() => import('./atoms/ReadingBar'))
 };
