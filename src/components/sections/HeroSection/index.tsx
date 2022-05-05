987a187d6288a1b08aa04133a17d96e22469592d
Stackbit Code Editor <projects@stackbit.com>
2022-05-05 20:12:30 +0000

    index.tsx: changed

--- src/components/sections/HeroSection/index.tsx
+++ src/components/sections/HeroSection/index.tsx
@@ -76,7 +76,7 @@ function HeroBody(props) {
                             loop: true,
                             wrapperClassName: 'text-primary',
                             skipAddStyles: true,
-                            cursorClassName: 'border',
+                            cursorClassName: 'border-primary',
                         }}
                     /></span>
                     
