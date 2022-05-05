4849ac9ece9438c3b071218aaa6fcc8eb1f80c8d
Stackbit Code Editor <projects@stackbit.com>
2022-05-05 17:33:30 +0000

    index.tsx: changed

---
+++ src/components/molecules/ReactType/index.tsx
@@ -0,0 +1,10 @@
+import TypeWriterEffect from 'react-typewriter-effect';
+
+ <TypeWriterEffect
+            textStyle={{ fontFamily: 'Red Hat Display' }}
+            startDelay={100}
+            cursorColor="black"
+            text="This is a single text"
+            typeSpeed={100}
+            scrollArea={myAppRef}
+          />
