c9e9bfbcef6def44f5f2379dd10255d7aefc28a1
Stackbit Code Editor <projects@stackbit.com>
2022-05-05 19:13:23 +0000

    index.tsx: changed

--- src/components/sections/HeroSection/index.tsx
+++ src/components/sections/HeroSection/index.tsx
@@ -110,7 +110,7 @@ function HeroActions(props) {
         >
             <div className={classNames('flex', 'flex-wrap', 'items-center', '-mx-2', mapStyles(styles))} data-sb-field-path=".actions">
                 {actions.map((action, index) => (
-                    <Action key={index} {...action} className="my-2 mx-2 lg:whitespace-nowrap " data-sb-field-path={`.${index}`} />
+                    <Action key={index} {...action} className="my-2 mx-2 lg:whitespace-nowrap border-yellow-400" data-sb-field-path={`.${index}`} />
                 ))}
             </div>
         </div>
