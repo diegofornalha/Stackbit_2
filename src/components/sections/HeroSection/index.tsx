37e1fcfd4c8d60714a05408fc0a7db3d3b7b5e27
Stackbit Code Editor <projects@stackbit.com>
2022-05-05 17:54:14 +0000

    index.tsx: changed

--- src/components/sections/HeroSection/index.tsx
+++ src/components/sections/HeroSection/index.tsx
@@ -57,14 +57,7 @@ function HeroBody(props) {
                 </h2>
             )}
 
-            {<h2 className={classNames('h1', styles.title ? mapStyles(styles.title) : null, { 'mt-4': props.badge?.label })} data-sb-field-path=".title" style={{ color: '#FFD662' }}>
-                <Typewriter
-                    options={{
-                        strings: ['CAFFÈ...', 'APEROL SPRITZ...', 'GELATO...', 'CAPPUCCINO...', 'THÈ...'],
-                        autoStart: true,
-                        loop: true,
-                    }}
-                /></h2>}
+            
 
             {subtitle && (
                 <p
