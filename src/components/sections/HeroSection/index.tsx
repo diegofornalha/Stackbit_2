39367f46f2e5bd811b772a18e006671b95f049ea
Stackbit Code Editor <projects@stackbit.com>
2022-05-05 17:56:29 +0000

    index.tsx: changed

--- src/components/sections/HeroSection/index.tsx
+++ src/components/sections/HeroSection/index.tsx
@@ -54,9 +54,7 @@ function HeroBody(props) {
     return (
         <>
             {title && (
-                <h2 className={classNames('h1', styles.title ? mapStyles(styles.title) : null)} data-sb-field-path=".title">
-                    {title}
-                </h2>
+                
 
                 
                 
