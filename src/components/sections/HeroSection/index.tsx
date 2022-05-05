f22b3232d25b0e6a5f7541dd35e885261e0bf328
Stackbit Code Editor <projects@stackbit.com>
2022-05-05 17:56:41 +0000

    index.tsx: changed

--- src/components/sections/HeroSection/index.tsx
+++ src/components/sections/HeroSection/index.tsx
@@ -54,7 +54,7 @@ function HeroBody(props) {
     return (
         <>
             {title && (
-                {<h2 className={classNames('h1', styles.title ? mapStyles(styles.title) : null, { 'mt-4': props.badge?.label })} data-sb-field-path=".title" style={{ color: '#FFD662' }}>
+                <h2 className={classNames('h1', styles.title ? mapStyles(styles.title) : null, { 'mt-4': props.badge?.label })} data-sb-field-path=".title" style={{ color: '#FFD662' }}>
                 <Typewriter
                     options={{
                         strings: ['CAFFÈ...', 'APEROL SPRITZ...', 'GELATO...', 'CAPPUCCINO...', 'THÈ...'],
