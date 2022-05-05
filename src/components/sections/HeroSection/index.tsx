3200218c87af2a476e02fd5079f84e70efe66c76
Stackbit Code Editor <projects@stackbit.com>
2022-05-05 18:41:02 +0000

    index.tsx: changed

--- src/components/sections/HeroSection/index.tsx
+++ src/components/sections/HeroSection/index.tsx
@@ -71,7 +71,7 @@ function HeroBody(props) {
                             strings: ['product manager', 'nerd', 'indie hacker', 'creator'],
                             autoStart: true,
                             loop: true,
-                            wrapperClassName: cose,
+                            wrapperClassName: yellow,
                             skipAddStyles: true,
                         }}
                     /></span>
