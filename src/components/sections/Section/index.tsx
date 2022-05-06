12026ce8ee9358186c4fc9805ad77653dee360ab
Stackbit Code Editor <projects@stackbit.com>
2022-05-06 15:17:36 +0000

    index.tsx: changed

--- src/components/sections/Section/index.tsx
+++ src/components/sections/Section/index.tsx
@@ -93,7 +93,6 @@ function SectionFullWidth(props: SectionProps) {
             <div className={classNames('flex', 'w-full', mapStyles({ justifyContent: styles.justifyContent ?? 'center' }))}>
                 <div className={classNames('relative', 'w-full', mapStyles({ width: styles.width ?? 'wide' }))}>{children}</div>
             </div>
-            <Newsletter />
         </div>
     );
 }
