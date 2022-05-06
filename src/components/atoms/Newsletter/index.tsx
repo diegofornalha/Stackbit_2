d9674fcb067db0f7e10348661a590aa02a472981
Stackbit Code Editor <projects@stackbit.com>
2022-05-06 12:30:08 +0000

    index.tsx: changed

--- src/components/atoms/Newsletter/index.tsx
+++ src/components/atoms/Newsletter/index.tsx
@@ -1,3 +1,17 @@
+// src/components/sections/AlertSection.tsx
+import React from 'react'
+
+const AlertSection = ({ text }) => {
+  return (
+    <div className="py-6 bg-primary">
+      <div className="container mx-auto">
+        <p className="text-center text-xl text-white">{text}</p>
+      </div>
+    </div>
+  )
+}
+
+export default AlertSection
 
 
 
