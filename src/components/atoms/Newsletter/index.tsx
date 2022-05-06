1382c7a86bc7aa5c9f1195a3cf5142bd7bc16805
Stackbit Code Editor <projects@stackbit.com>
2022-05-06 15:13:36 +0000

    index.tsx: changed

--- src/components/atoms/Newsletter/index.tsx
+++ src/components/atoms/Newsletter/index.tsx
@@ -3,7 +3,7 @@ import Script from 'next/script'
 export default function Home() {
   return (
     <>
-      <Script src="https://www.google-analytics.com/analytics.js" />
+      <Script  />
     </>
   )
 }
