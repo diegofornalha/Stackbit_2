9e63787b7cd97dae4eb623caf959ec5b9a4ed01b
Stackbit Code Editor <projects@stackbit.com>
2022-05-06 09:15:16 +0000

    index.tsx: changed

--- src/components/sections/HeroSection/index.tsx
+++ src/components/sections/HeroSection/index.tsx
@@ -28,7 +28,7 @@ export default function HeroSection(props) {
                     <HeroActions actions={actions} styles={styles.actions} hasTopMargin={!!(title || subtitle || text)} />
                 </div>
                 {media && (
-                    <div className="flex-1 w-full absolute right-0">
+                    <div className="flex-1 w-full absolute left-0">
                         <HeroMedia media={media} />
                     </div>
                 )}
