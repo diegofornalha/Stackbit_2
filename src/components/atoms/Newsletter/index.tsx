72c8dfb4d642ba3ea9a3a17de079d9a709fb0c1b
Stackbit Code Editor <projects@stackbit.com>
2022-05-06 12:38:38 +0000

    index.tsx: changed

--- src/components/atoms/Newsletter/index.tsx
+++ src/components/atoms/Newsletter/index.tsx
@@ -1,7 +1,7 @@
 // src/components/sections/AlertSection.tsx
 import React from 'react'
 
-const Newsletter = ({ text }) => {
+const Newsletter = ({ script }) => {
   return (
     <script async data-uid="c4fbdd0325" src="https://exceptional-author-9523.ck.page/c4fbdd0325/index.js"></script>
   )
