62e7fe13ecf848ac0cda997a44f1ac5fa82a885e
Stackbit Code Editor <projects@stackbit.com>
2022-05-06 12:53:11 +0000

    index.tsx: changed

--- src/components/atoms/Newsletter/index.tsx
+++ src/components/atoms/Newsletter/index.tsx
@@ -4,7 +4,7 @@ import Script from 'next/script'
 export default function Newsletter() {
   return (
     <>
-      <Script src="https://www.google-analytics.com/analytics.js" />
+      <<script async data-uid="c4fbdd0325" src="https://exceptional-author-9523.ck.page/c4fbdd0325/index.js"></script>/>
     </>
   )
 }
