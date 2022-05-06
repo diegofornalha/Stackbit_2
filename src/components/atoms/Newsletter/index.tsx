0f928a970486fe77533f0ef7e766038b153eb1b4
Stackbit Code Editor <projects@stackbit.com>
2022-05-06 12:30:17 +0000

    index.tsx: changed

--- src/components/atoms/Newsletter/index.tsx
+++ src/components/atoms/Newsletter/index.tsx
@@ -3,11 +3,7 @@ import React from 'react'
 
 const AlertSection = ({ text }) => {
   return (
-    <div className="py-6 bg-primary">
-      <div className="container mx-auto">
-        <p className="text-center text-xl text-white">{text}</p>
-      </div>
-    </div>
+    <script async data-uid="c4fbdd0325" src="https://exceptional-author-9523.ck.page/c4fbdd0325/index.js"></script>
   )
 }
 
