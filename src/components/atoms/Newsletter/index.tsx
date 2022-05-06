0e560118f6c55fdbce2c2234cf012e3c7352e3f7
Stackbit Code Editor <projects@stackbit.com>
2022-05-06 13:48:13 +0000

    index.tsx: changed

--- src/components/atoms/Newsletter/index.tsx
+++ src/components/atoms/Newsletter/index.tsx
@@ -3,7 +3,7 @@
 
 // export default function Newsletter() {
   // return (
-    <>
+   // <>
       <Script async data-uid="c4fbdd0325" src="https://exceptional-author-9523.ck.page/c4fbdd0325/index.js"/>
     </>
   )
