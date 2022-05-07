237a23d742e5e466ce92c925987d9a7b18f4ae7e
Stackbit Code Editor <projects@stackbit.com>
2022-05-07 15:20:47 +0000

    index.tsx: changed

--- src/components/atoms/Readingbar/index.tsx
+++ src/components/atoms/Readingbar/index.tsx
@@ -4,11 +4,8 @@ import Script from 'next/script';
 
 const Readingbar = ({ text }) => {
   return (
-    <div className="py-6 bg-primary">
-      <div className="container mx-auto">
-        <p className="text-center text-xl text-white">{text}</p>
-      </div>
-    </div>
+    <div id="reading-progress" class="border-yellow squiggle"><div id="reading-progress-fill"></div></div>
+
   
   
   
