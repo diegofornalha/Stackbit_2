43a136f7614b19df50505fbd664f3f378f86c031
Stackbit Code Editor <projects@stackbit.com>
2022-05-07 15:09:41 +0000

    index.tsx: changed

--- src/components/atoms/Readingbar/index.tsx
+++ src/components/atoms/Readingbar/index.tsx
@@ -3,19 +3,16 @@ import Script from 'next/script';
 
 
 
-export default function ReadingBar() {
-  return (
-    <>
-    <div id="reading-progress" class="border-yellow squiggle"><div id="reading-progress-fill"></div></div>
-
-<Script
-const readingProgress = {document.querySelector('#reading-progress-fill');
-document.addEventListener('scroll', function(e) {
-  let w = (document.body.scrollTop || document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
-  readingProgress.style.setProperty('width', w + '%');
-}) />  
-  
-}
+var NewComponent = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <div id="reading-progress" className="border-yellow squiggle"><div id="reading-progress-fill" /></div>
+        <style dangerouslySetInnerHTML={{__html: "\n#reading-progress {\n  position: fixed;\n  width: 100%;\n  height: 5px;\n  z-index: 9999;\n  top: 0;\n  left: 0;\n}\n#reading-progress-fill {\n  height: 5px;\n  width: 1%;\n}\n/* Theme */\n#reading-progress-fill {\n  -webkit-transition: width 100ms ease;\n  -o-transition: width 100ms ease;\n  transition: width 100ms ease;\n  background-color: #FEE715;\n}\n" }} />
+      </div>
+    );
+  }
+});
 
 
 
