88cb2d94d4b2df4b45ee4ca5ca806fda330d6987
Stackbit Code Editor <projects@stackbit.com>
2022-05-07 15:07:30 +0000

    index.tsx: changed

--- src/components/atoms/Readingbar/index.tsx
+++ src/components/atoms/Readingbar/index.tsx
@@ -8,8 +8,12 @@ export default function Newsletter() {
     <>
     <div id="reading-progress" class="border-yellow squiggle"><div id="reading-progress-fill"></div></div>
 
-      <Script async data-uid="c4fbdd0325" src="https://exceptional-author-9523.ck.page/c4fbdd0325/index.js" />
-    </>
+<Script
+const readingProgress = document.querySelector('#reading-progress-fill');
+document.addEventListener('scroll', function(e) {
+  let w = (document.body.scrollTop || document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
+  readingProgress.style.setProperty('width', w + '%');
+}) />    </>
   )
 }
 
