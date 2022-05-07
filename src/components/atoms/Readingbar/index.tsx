2959a9aa2b275d67234d8c8383c05ac959176567
Stackbit Code Editor <projects@stackbit.com>
2022-05-07 15:08:39 +0000

    index.tsx: changed

--- src/components/atoms/Readingbar/index.tsx
+++ src/components/atoms/Readingbar/index.tsx
@@ -14,7 +14,7 @@ document.addEventListener('scroll', function(e) {
   let w = (document.body.scrollTop || document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
   readingProgress.style.setProperty('width', w + '%');
 }) />  
-  )
+  
 }
 
 
