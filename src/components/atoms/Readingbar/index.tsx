8467bded702960ce025fa31a23b05feccf313c63
Stackbit Code Editor <projects@stackbit.com>
2022-05-07 15:06:24 +0000

    index.tsx: changed

--- src/components/atoms/Readingbar/index.tsx
+++ src/components/atoms/Readingbar/index.tsx
@@ -6,6 +6,8 @@ import Script from 'next/script';
 export default function Newsletter() {
   return (
     <>
+    <div id="reading-progress" class="border-yellow squiggle"><div id="reading-progress-fill"></div></div>
+
       <Script async data-uid="c4fbdd0325" src="https://exceptional-author-9523.ck.page/c4fbdd0325/index.js" />
     </>
   )
