affc587c09836947d09e7c059ce20d310d1b5008
Stackbit Code Editor <projects@stackbit.com>
2022-05-07 15:08:13 +0000

    index.tsx: changed

--- src/components/atoms/Readingbar/index.tsx
+++ src/components/atoms/Readingbar/index.tsx
@@ -9,7 +9,7 @@ export default function ReadingBar() {
     <div id="reading-progress" class="border-yellow squiggle"><div id="reading-progress-fill"></div></div>
 
 <Script
-const readingProgress = document.querySelector('#reading-progress-fill');
+const readingProgress = {document.querySelector('#reading-progress-fill');
 document.addEventListener('scroll', function(e) {
   let w = (document.body.scrollTop || document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
   readingProgress.style.setProperty('width', w + '%');
