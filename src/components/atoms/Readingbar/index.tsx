83e5c6d9d2e1cd8a8bcdf00179908c74c656ae91
Stackbit Code Editor <projects@stackbit.com>
2022-05-07 15:20:35 +0000

    index.tsx: changed

--- src/components/atoms/Readingbar/index.tsx
+++ src/components/atoms/Readingbar/index.tsx
@@ -16,3 +16,34 @@ export default AlertSection
 
 
 
+<div id="reading-progress" class="border-yellow squiggle"><div id="reading-progress-fill"></div></div>
+
+<script>
+const readingProgress = document.querySelector('#reading-progress-fill');
+document.addEventListener('scroll', function(e) {
+  let w = (document.body.scrollTop || document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
+  readingProgress.style.setProperty('width', w + '%');
+});
+</script>
+
+<style>
+#reading-progress {
+  position: fixed;
+  width: 100%;
+  height: 5px;
+  z-index: 9999;
+  top: 0;
+  left: 0;
+}
+#reading-progress-fill {
+  height: 5px;
+  width: 1%;
+}
+/* Theme */
+#reading-progress-fill {
+  -webkit-transition: width 100ms ease;
+  -o-transition: width 100ms ease;
+  transition: width 100ms ease;
+  background-color: #FEE715;
+}
+</style>
