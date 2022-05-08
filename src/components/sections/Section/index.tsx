34071af965589b196448c3f11ae222d51d2a55a7
Stackbit Code Editor <projects@stackbit.com>
2022-05-08 17:54:19 +0000

    index.tsx: changed

--- src/components/sections/Section/index.tsx
+++ src/components/sections/Section/index.tsx
@@ -67,6 +67,7 @@ function SectionFullWidth(props: SectionProps) {
     const classSuffix = type && type.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
     return (
         <div
+        
             id={elementId || null}
             className={classNames(
                 'sb-component',
