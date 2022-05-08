dc10f6b801d81dc0e8d4ae007e5dd3c32e6d227e
Stackbit Code Editor <projects@stackbit.com>
2022-05-08 18:25:12 +0000

    index.tsx: changed

--- src/components/sections/LabelsSection/index.tsx
+++ src/components/sections/LabelsSection/index.tsx
@@ -11,7 +11,7 @@ export default function LabelsSection(props) {
     const { type, elementId, colors, title, subtitle, items = [], styles = {}, 'data-sb-field-path': fieldPath } = props;
     return (
         <Section type={type} elementId={elementId} colors={colors} styles={styles.self} data-sb-field-path={fieldPath}>
-            {elementId?.backgroundImage && <BackgroundImage {...page?.backgroundImage} />}
+            {elementId?.backgroundImage && <BackgroundImage {...elementId?.backgroundImage} />}
             {title && (
                 <h2 className={classNames(styles.title ? mapStyles(styles.title) : null)} data-sb-field-path=".title">
                     {title}
