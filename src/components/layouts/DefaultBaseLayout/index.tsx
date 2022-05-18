b05725e1a901c8a75b3a5ba2047184dd5ae50d88
Stackbit Code Editor <projects@stackbit.com>
2022-05-18 13:03:03 +0000

    index.tsx: changed

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -37,7 +37,6 @@ export default function DefaultBaseLayout(props) {
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'G-Y8CEBYNYN3');
 </script>
                     
