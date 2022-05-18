37867f1b2dfda934401150e22f9f5a81c49665a3
Stackbit Code Editor <projects@stackbit.com>
2022-05-18 13:02:35 +0000

    index.tsx: changed

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -32,6 +32,15 @@ export default function DefaultBaseLayout(props) {
                     })}
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
                     {site.favicon && <link rel="icon" href={site.favicon} />}
+                    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y8CEBYNYN3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Y8CEBYNYN3');
+</script>
                     
                 </Head>
                 {site.header && <Header {...site.header} annotationPrefix={siteMeta.id} />}
