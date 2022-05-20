9455a06277c7759460c515303d0ed507f08da245
Stackbit Code Editor <projects@stackbit.com>
2022-05-20 23:18:00 +0000

    index.tsx: changed

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -50,7 +50,7 @@ dangerouslySetInnerHTML={{
     __javascript:  `
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  gtag('js', new Date());,
 
   gtag('config', 'G-Y8CEBYNYN3');}}>
 </script>  
