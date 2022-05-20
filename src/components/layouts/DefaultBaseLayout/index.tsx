f0d19bf5b9a397413a8bd5af7f58f721360e17c8
Stackbit Code Editor <projects@stackbit.com>
2022-05-20 23:22:13 +0000

    index.tsx: changed

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -39,11 +39,10 @@ export default function DefaultBaseLayout(props) {
                  <Script strategy="lazyOnload">
                 {`
                     window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Y8CEBYNYN3');
                 `}
             </Script>
                 
