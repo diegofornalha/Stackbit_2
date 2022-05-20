493e1f08f5f4d5d555824a621d93298f1558afba
Stackbit Code Editor <projects@stackbit.com>
2022-05-20 23:15:40 +0000

    index.tsx: changed

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -44,7 +44,14 @@ export default function DefaultBaseLayout(props) {
             }}
           ></script>
 
-          
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Y8CEBYNYN3"/>
+<Script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-Y8CEBYNYN3');
+</Script>  
                 </Head>
                 {site.header && <Header {...site.header} annotationPrefix={siteMeta.id} />}
                 {props.children}
