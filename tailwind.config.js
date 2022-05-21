9add1151e6c9a6953300b098d2fb4af3db9b2b8d
Stackbit Code Editor <projects@stackbit.com>
2022-05-21 11:34:34 +0000

    tailwind.config.js: changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
                 base: themeStyle.baseFontSize
             },
             fontFamily: {
-                
+                base: themeStyle.
             },
             colors: {
                 light: themeStyle.light,
