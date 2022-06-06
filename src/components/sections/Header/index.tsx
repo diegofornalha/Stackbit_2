1a7fe469047a25aff610458a8d884da5959607a4
Stackbit Code Editor <projects@stackbit.com>
2022-06-06 14:05:23 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -20,7 +20,7 @@ export default function Header(props) {
 
             <div
                 className={classNames('mx-auto', mapMaxWidthStyles(headerWidth), {
-                    'xl:border-l xl:border-r border-current': headerWidth === 'narrow',
+                    'xl:border-l xl:border-r border-current border-b': headerWidth === 'narrow',
                     '2xl:border-l 2xl:border-r border-current': headerWidth === 'wide'
                 })}
             >
