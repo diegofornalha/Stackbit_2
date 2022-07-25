994f2808f592391e47dc96f472d7891073abbab3
Stackbit Code Editor <projects@stackbit.com>
2022-07-25 12:21:11 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -93,7 +93,7 @@ function HeaderVariantB(props) {
             )}
 
             {primaryActions.length > 0 && (
-                                <ul className="hidden lg:flex border-l border-current divide-x divide-current ml-auto" data-sb-field-path=".primaryLinks">
+                <ul className="hidden lg:flex border-l border-current divide-x divide-current ml-auto" data-sb-field-path=".primaryLinks">
 
             )}
 
