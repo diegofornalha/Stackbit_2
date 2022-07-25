3c0877b87b60eb25f407b625040d71e69fdfa7b3
Stackbit Code Editor <projects@stackbit.com>
2022-07-25 12:16:50 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -35,6 +35,7 @@ export default function Header(props) {
                     logo={logo}
                     primaryLinks={primaryLinks}
                     socialLinks={socialLinks}
+                    ac
                 />
             </div>
         </header>
