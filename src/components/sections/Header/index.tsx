f516c9aa8809d67250c206c162c343ce598b129d
Stackbit Code Editor <projects@stackbit.com>
2022-07-25 12:35:28 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -94,6 +94,8 @@ function HeaderVariantB(props) {
                 </ul>
             )}
 
+            
+
             {socialLinks.length > 0 && (
                 <ul
                     className={classNames('hidden', 'lg:flex', 'border-l', 'border-current', {
