ffaca803786f458ac9627f8e9788c6d3579129d5
Stackbit Code Editor <projects@stackbit.com>
2022-07-25 19:08:46 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -87,7 +87,7 @@ function HeaderVariantB(props) {
         <div className="flex items-stretch relative">
             <SiteLogoLink {...logoProps} />
             {primaryLinks.length > 0 && (
-                <ul className="hidden lg:flex divide-x divide-current ml-auto" data-sb-field-path=".primaryLinks">
+                <ul className="hidden lg:flex  ml-auto" data-sb-field-path=".primaryLinks">
                     <ListOfLinks links={primaryLinks} inMobileMenu={false} />
                 </ul>
             )}
