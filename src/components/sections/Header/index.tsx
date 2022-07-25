bbc5094a2eec6c5fd1a2a66c3aa88193e2aacf9f
Stackbit Code Editor <projects@stackbit.com>
2022-07-25 12:09:47 +0000

    index.tsx: changed

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -87,9 +87,7 @@ function HeaderVariantB(props) {
             <SiteLogoLink {...logoProps} />
             {primaryLinks.length > 0 && (
                 <ul className="hidden lg:flex border-l border-current divide-x divide-current ml-auto" data-sb-field-path=".primaryLinks">
-                    <ListOfLinks links={primaryLinks} inMobileMenu={false} />
-                <Actions />
-                
+                    <ListOfLinks links={primaryLinks} inMobileMenu={false} />                
                 </ul>
             )}
 
